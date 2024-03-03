6f0b19ce810bef1c69c3c874885897bd8c77165e
Lilly Grella <60951320+Lgrella@users.noreply.github.com>
2024-03-02 23:12:55 -0500

    Update s3 creation code

--- lib/week3-stack.ts
+++ lib/week3-stack.ts
@@ -1,19 +1,25 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
-export class Week3Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class Week3Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'Week3Queue', {
-      visibilityTimeout: Duration.seconds(300)
+    // I have created a new S3 bucket with the following properties:
+    const s3Bucket = new s3.Bucket(this, 'MyS3Bucket', {
+      encryptionKey: new kms.Key(this, 's3BucketKMSKey'),
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_ENFORCED,
+      versioned: true,
     });
 
-    const topic = new sns.Topic(this, 'Week3Topic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    s3Bucket.grantRead(new iam.AccountRootPrincipal());
   }
 }
+
+const app = new cdk.App();
+new Week3Stack(app, 'S3BucketStack');
+app.synth();
